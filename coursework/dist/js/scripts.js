ceea03c58b5e24792271f8f2def8563257426e05
alsoderg90 <alexander.sodergard@gmail.com>
2021-12-18 04:27:20 +0200

    frontpage & about page

--- coursework/dist/js/scripts.js
+++ coursework/dist/js/scripts.js
@@ -2,7 +2,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
@@ -29,3 +28,46 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+// Scroll based animations on front page
+
+var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl2 = new TimelineMax();
+const controller = new ScrollMagic.Controller();
+
+
+// Timeline animations
+tl.from("#blockquote", .5, {x:200, opacity:0});
+tl.from("#span", 1, {width:0}, "-1=");
+tl.from("#first", 1, {x:100, y:200, opacity:0}, "=-1");
+tl.from("#second", 1, {x:-200, y:0, opacity:0}, "=-.7");
+
+const scene = new ScrollMagic.Scene({
+  triggerElement: ".sticky", 
+  triggerHook: "onLeave",
+  duration: "100%"
+})
+.setPin(".sticky")
+.setTween(tl)
+  .addTo(controller);
+
+tl2.from("#box", .5,{opacity: 10, scale: 0}, "=-.8");
+tl2.to("#box", .5 ,{
+  right: "35%",
+  bottom: "50%",
+  scale: 1.3,
+  borderColor: "white",
+  borderWidth: 5,
+  boxShadow: '1px 1px 0px 0px rgba(0,0,0,.09)'
+});
+
+const scene2 = new ScrollMagic.Scene({
+  triggerElement: "#blockquote"
+})
+ .setTween(tl2)
+   .addTo(controller);
+
+function updatePercentage() {
+  tl.progress();
+}
+
